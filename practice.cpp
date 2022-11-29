9b55a6688682e8d1e07bc09e541d1955090fbe4d
Kiluk <119402295+Kiluk@users.noreply.github.com>
2022-11-29 18:09:31 +0100

    assign

--- practice.cpp
+++ practice.cpp
@@ -10,14 +10,24 @@ int main()
 {
     std::vector < int > dane;
     dane.push_back( 5 );
-    dane.push_back( 2 );
+    dane.push_back( 4 );
     dane.push_back( 3 );
+    dane.push_back( 2 );
+    dane.push_back( 1 );
+    dane.push_back( 0 );
     dane.push_back( 1 );
    
     for( size_t i = 0; i < dane.size(); i++ )
          cout <<  dane[ i ] << ", " ;
    
-    printf( "\n\nKoniec\n" );
+    cout << "\nKoniec\n";
+
+    dane.assign(dane.begin() +2,dane.begin() +6);
+
+
+    for( size_t i = 0; i < dane.size(); i++ )
+         cout <<  dane[ i ] << ", " ;
+   
     return 0;
 }
 
